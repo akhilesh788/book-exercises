e5d9c85cef8876be119e99825d021d88f0fbdc3b
Akhilesh H. Mavlankar <akhilesh788@gmail.com>
2019-01-09 21:16:39 +0530

    More practice

--- chapter-10-exercises/exercise-1/exercise.R
+++ chapter-10-exercises/exercise-1/exercise.R
@@ -3,27 +3,38 @@
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
 
+scores<-c(40,50,60,70)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
 
+allowed<-c(10,20,30,40)
 
 # Combine your two vectors into a dataframe called `games`
 
-
+games<-data.frame(scores,allowed,stringsAsFactors = FALSE)
+games
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
 
+games$diff<-games$scores-games$allowed
+games
 
 # Create a new column "won" which is TRUE if the Seahawks won the game
 
+games[["Won"]]<-scores>allowed
+games
 
 # Create a vector of the opponent names corresponding to the games played
 
+games$Oppenents<-c("aa","bb","cc","dd")
+games
 
 # Assign your dataframe rownames of their opponents
 
+rownames(games)<-games$Oppenents
+games
 
 # View your data frame to see how it has changed!
-
+View(games)
--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -4,59 +4,82 @@
 # Hint: use the `paste()` function and vector recycling to add a number to the word
 # "Employee"
 
+emp<-paste("Employee",1:100)
+emp
 
 # Create a vector of 100 random salaries for the year 2017
 # Use the `runif()` function to pick random numbers between 40000 and 50000
 
+?runif
+salaries_vector<-runif(100,40000,50000)
+salaries_vector
 
 # Create a vector of 100 annual salary adjustments between -5000 and 10000.
 # (A negative number represents a salary decrease due to corporate greed)
 # Again use the `runif()` function to pick 100 random numbers in that range.
 
+adjustments<-runif(100,-5000,10000)
+adjustments
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
+
+salaries<-data.frame(emp,salaries_vector,adjustments,stringsAsFactors = FALSE)
+salaries
 
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
 
+salaries$adjusted_salaries<-salaries$salaries_vector+salaries$adjustments
+salaries
+
 
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
 
+salaries$raise_or_not<-salaries$adjustments>0
+head(salaries)
 
 
 ### Retrieve values from your data frame to answer the following questions
 ### Note that you should get the value as specific as possible (e.g., a single
 ### cell rather than the whole row!)
 
-# What was the 2018 salary of Employee 57
 
+# What was the 2018 salary of Employee 57
+rownames(salaries)<-salaries$emp
+head(salaries)
+
+print(salaries["Employee 57","adjusted_salaries"])
 
 # How many employees got a raise?
-
+length(salaries$adjustments[salaries$adjustments>0])
 
 # What was the dollar value of the highest raise?
-
+max(salaries$adjustments)
+max_raise<-max(salaries$adjustments)
 
 # What was the "name" of the employee who received the highest raise?
-
+salaries$emp[salaries$adjustments==max_raise]
 
 # What was the largest decrease in salaries between the two years?
-
+min(salaries$adjustments)
+max_decrease<-min(salaries$adjustments)
 
 # What was the name of the employee who recieved largest decrease in salary?
 
+salaries$emp[salaries$adjustments==max_decrease]
 
 # What was the average salary change?
-
+mean(salaries$adjustments)
 
 # For people who did not get a raise, how much money did they lose on average?
-
-
+v1<-salaries[salaries$adjustments<0 , "adjustments"]
+print(mean(v1))
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
 
 # Write a .csv file of your salary data to your working directory
-
+?write.csv
+write.csv(salaries,"Salary_data2.csv",row.names = FALSE)
